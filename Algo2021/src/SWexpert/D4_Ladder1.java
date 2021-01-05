ccf207f2f831f1d41baef11072497a2d18055c31
김소희 <sohi9732@naver.com>
2021-01-05 20:11:55 +0900

    commit

---
+++ Algo2021/src/SWexpert/D4_Ladder1.java
@@ -0,0 +1,13 @@
+package SWexpert;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class D4_Ladder1 {
+
+	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
