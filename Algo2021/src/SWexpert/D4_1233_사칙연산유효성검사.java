44bbd9c25463e5ab0130f3ebfdcce5c554ee1ccf
김소희 <sohi9732@naver.com>
2021-01-27 07:50:22 +0900

    0126 D4_1233_사칙연산유효성검사

--- Algo2021/src/SWexpert/D4_1233_사칙연산유효성검사.java
+++ Algo2021/src/SWexpert/D4_1233_사칙연산유효성검사.java
@@ -5,24 +5,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class D4_1233_사칙연산유효성검사 {
-	
-	static class Node {
-		int index;
-		char op;
-		int num;
-		int left;
-		int right;
-		public Node(int index, char op, int left, int right) {
-			this.index = index;
-			this.op = op;
-			this.left = left;
-			this.right = right;
-		}
-		public Node(int index, int num) {
-			this.index = index;
-			this.num = num;
-		}
-	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -33,24 +15,17 @@ public class D4_1233_사칙연산유효성검사 {
 			st = new StringTokenizer(in.readLine(), " ");
 			int N = Integer.parseInt(st.nextToken());
 			
-			Node[] arr = new Node[N];
-			
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(in.readLine(), " ");
-				int index = Integer.parseInt(st.nextToken())-1;
+				st.nextToken();
 				if(st.countTokens() == 3) {
-					char op = st.nextToken().charAt(0);
-					int left = Integer.parseInt(st.nextToken());
-					int right = Integer.parseInt(st.nextToken());
-					arr[i] = new Node(index, op, left, right);
+					st.nextToken();
+					st.nextToken();
+					st.nextToken();
 				}
 				else {
 					char ch = st.nextToken().charAt(0);
-					if(Character.isDigit(ch)) {
-						int num = ch-'0';
-						arr[i] = new Node(index, num);
-					}
-					else {
+					if(!Character.isDigit(ch)) {
 						answer = 0;
 					}
 				}
