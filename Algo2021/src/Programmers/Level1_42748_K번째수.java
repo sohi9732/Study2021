dc45c3b9648198e1a9df9a6038c3a8691f6ec753
김소희 <sohi9732@naver.com>
2021-02-04 02:52:51 +0900

    0203 Level1_42748_K번째수

--- Algo2021/src/Programmers/Level1_42748_K번째수.java
+++ Algo2021/src/Programmers/Level1_42748_K번째수.java
@@ -3,7 +3,7 @@ package Programmers;
 import java.util.*;
 
 public class Level1_42748_K번째수 {
-
+	
 	public static void main(String[] args) {
 		int[] array = {1, 5, 2, 6, 3, 7, 4};
 		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
@@ -12,11 +12,11 @@ public class Level1_42748_K번째수 {
 	}
 	
     public static int[] solution(int[] array, int[][] commands) {
-		int[] answer = new int[commands.length];
-		int index;
-		
-		for(int i = 0; i < commands.length; i++) {
-		    index = 0;
+    	int[] answer = new int[commands.length];
+    	int index;
+    	
+    	for(int i = 0; i < commands.length; i++) {
+    		index = 0;
 		    int[] tmp = new int[commands[i][1]-commands[i][0]+1];
 		    for(int j = commands[i][0]-1; j < commands[i][1]; j++) {
 		        tmp[index++] = array[j];
