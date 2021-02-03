d33ae91276f811d9ea54e3006609efbd8dde620c
김소희 <sohi9732@naver.com>
2021-02-04 02:49:54 +0900

    0203 Level1_42576_완주하지못한선수

--- Algo2021/src/Programmers/Level1_42576_완주하지못한선수.java
+++ Algo2021/src/Programmers/Level1_42576_완주하지못한선수.java
@@ -21,22 +21,22 @@ public class Level1_42576_완주하지못한선수 {
 	
 	public static String solution(String[] participant, String[] completion) {
 		String answer = "";
-        boolean found = false;
-        
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        
-        for(int i = 0; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                answer = participant[i];
-                found = true;
-                break;
-            }
-        }
-        
-        if(!found) answer = participant[participant.length-1];
-        
-        return answer;
-    }
+		boolean found = false;
+		
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		for(int i = 0; i < completion.length; i++) {
+		    if(!participant[i].equals(completion[i])) {
+		        answer = participant[i];
+		        found = true;
+		        break;
+		    }
+		}
+		
+		if(!found) answer = participant[participant.length-1];
+		
+		return answer;
+	}
 
 }
