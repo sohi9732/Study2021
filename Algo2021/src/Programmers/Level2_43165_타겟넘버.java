a5ce33aca5e01cbd62bece2268553dbaa594e7b7
김소희 <sohi9732@naver.com>
2021-03-06 18:08:58 +0900

    02028 Level2_43165_타겟넘버

---
+++ Algo2021/src/Programmers/Level2_43165_타겟넘버.java
@@ -0,0 +1,42 @@
+package Programmers;
+
+public class Level2_43165_타겟넘버 {
+
+	static int N;
+	static boolean[] isSelected;
+	static int targetNum, answer;
+	
+	public static void main(String[] args) {
+		int[] numbers = {1, 1, 1, 1, 1};
+		System.out.println(solution(numbers, 3));
+	}
+	
+	public static int solution(int[] numbers, int target) {
+		targetNum = target;
+		
+		N = numbers.length;
+		isSelected = new boolean[N];
+		generateSubset(0, numbers);
+		
+		return answer;
+	}
+	
+	public static void generateSubset(int cnt, int[] numbers) {
+		if(cnt == N) {
+			int sum = 0;
+			for(int i = 0; i < N; i++) {
+				if(isSelected[i]) sum += numbers[i];
+				else sum -= numbers[i];
+			}
+			if(sum == targetNum) answer++;
+			return;
+		}
+		
+		isSelected[cnt] = true;
+		generateSubset(cnt+1, numbers);
+		
+		isSelected[cnt] = false;
+		generateSubset(cnt+1, numbers);
+	}
+
+}
