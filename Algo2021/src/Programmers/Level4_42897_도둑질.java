5aaec52710385239a8526fc5f8c1ef61dcdef107
김소희 <sohi9732@naver.com>
2021-04-01 16:05:48 +0900

    0315 Level4_42897_도둑질

---
+++ Algo2021/src/Programmers/Level4_42897_도둑질.java
@@ -0,0 +1,30 @@
+package Programmers;
+
+public class Level4_42897_도둑질 {
+
+	public static void main(String[] args) {
+		int[] money = {1, 2, 3, 1};
+		System.out.println(solution(money));
+	}
+	
+	public static int solution(int[] money) {
+		int answer = 0;
+		int[] dp = new int[money.length];
+		
+		dp[0] = dp[1] = money[0]; //첫번째 집 선택
+		for(int i = 2; i < money.length-1; i++) { //마지막 집 비선택
+			dp[i] = Math.max(dp[i-2]+money[i], dp[i-1]);
+		}
+		answer = dp[money.length-2];
+		
+		dp[0] = 0; //첫번째 집 비선택
+		dp[1] = money[1];
+		for(int i = 2; i < money.length; i++) {
+			dp[i] = Math.max(dp[i-2]+money[i], dp[i-1]);
+		}
+		answer = Math.max(answer, dp[money.length-1]);
+		
+		return answer;
+	}
+
+}
