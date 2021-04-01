ddf1170348b6f3079560ff9f6fce9f9bb776172b
김소희 <sohi9732@naver.com>
2021-04-01 22:02:28 +0900

    21/03/04 테스트 케이스 변경 후 코드 수정

--- Algo2021/src/Programmers/Level2_42577_전화번호목록.java
+++ Algo2021/src/Programmers/Level2_42577_전화번호목록.java
@@ -1,5 +1,7 @@
 package Programmers;
 
+import java.util.*;
+
 public class Level2_42577_전화번호목록 {
 
 	public static void main(String[] args) {
@@ -14,12 +16,11 @@ public class Level2_42577_전화번호목록 {
 	public static boolean solution(String[] phone_book) {
 		boolean answer = true;
 		
-		L:for(int i = 0; i < phone_book.length-1; i++) {
-			for(int j = i+1; j < phone_book.length; j++) {
-				if(phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i])) {
-					answer = false;
-					break L;
-				}
+		Arrays.sort(phone_book);
+		for(int i = 0; i < phone_book.length-1; i++) {
+			if(phone_book[i+1].startsWith(phone_book[i])) {
+				answer = false;
+				break;
 			}
 		}
 		
