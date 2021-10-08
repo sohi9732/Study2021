e95e5b43503561a47f0f1bb23cfe1f7337d5ef32
김소희 <sohi9732@naver.com>
2021-10-08 16:03:46 +0900

    Basic

--- Algo2021/src/Basic/HashMap1_BasicTest.java
+++ Algo2021/src/Basic/HashMap1_BasicTest.java
@@ -8,7 +8,7 @@ public class HashMap1_BasicTest {
 	public static void main(String[] args) {
 		HashMap<String, Integer> map = new HashMap<>();
 
-		System.out.println("1. put() 메소드를 이용한 요소 저장");
+		System.out.println("1. put() 메서드를 이용한 요소 저장");
 		map.put("Apple", 3);
 		map.put("Banana", 2);
 		map.put("Orange", 1);
@@ -19,7 +19,7 @@ public class HashMap1_BasicTest {
 
 		//V remove(Object key)
 		System.out.println("//V remove(Object key)");
-		System.out.println("2. remove() 메소드를 이용한 요소 제거 (*제거하면서 값 반환)");
+		System.out.println("2. remove() 메서드를 이용한 요소 제거 (*제거하면서 값 반환)");
 		System.out.println("'Melon'키 제거 후 반환 값 : " + map.remove("Melon"));
 		System.out.println("'Kiwi'키 제거 후 반환 값 : " + map.remove("Kiwi"));
 		System.out.println();
@@ -27,35 +27,35 @@ public class HashMap1_BasicTest {
 
 		//boolean remove(Object key, Object value)
 		System.out.println("//boolean remove(Object key, Object value)");
-		System.out.println("3. remove() 메소드를 이용한 요소 제거 (*기존 값을 올바르게 입력할 경우 가능)");
+		System.out.println("3. remove() 메서드를 이용한 요소 제거 (*기존 값을 올바르게 입력할 경우 가능)");
 		System.out.println("'Peach'키 값 2로 제거 가능한가? " + map.remove("Peach", 2));
 		System.out.println("'Peach'키 값 4로 제거 가능한가? " + map.remove("Peach", 4));
 		System.out.println();
 		printMap(map);
 		
-		System.out.println("4. keySet() 메소드를 이용한 키로 구성된 Set 객체 반환");
+		System.out.println("4. keySet() 메서드를 이용한 키로 구성된 Set 객체 반환");
 		System.out.println(map.keySet());
 		System.out.println();
 		
-		System.out.println("5. size() 메소드를 이용한 요소의 총 개수");
+		System.out.println("5. size() 메서드를 이용한 요소의 총 개수");
 		System.out.println("Map의 크기 : " + map.size());
 		System.out.println();
 		
-		System.out.println("6. get() 메소드를 이용하여 키에 대응하는 값 반환");
+		System.out.println("6. get() 메서드를 이용하여 키에 대응하는 값 반환");
 		System.out.println("'Apple'키의 값 : " + map.get("Apple"));
 		System.out.println("'Kiwi'키의 값 : " + map.get("Kiwi")); //없으면 null을 반환
 		System.out.println();
 		
 		//V replace(K key, V value)
 		System.out.println("//V replace(K key, V value)");
-		System.out.println("7. replace() 메소드를 이용하여 키에 대응하는 값 변환");
+		System.out.println("7. replace() 메서드를 이용하여 키에 대응하는 값 변환");
 		System.out.println("기존 'Blueberry'키의 값 : " + map.replace("Blueberry", 5));
 		System.out.println("변환 후 'Blueberry'키의 값 : " + map.get("Blueberry"));
 		System.out.println();
 		
 		//boolean replace(K key, V oldValue, V newValue)
 		System.out.println("//boolean replace(K key, V oldValue, V newValue)");
-		System.out.println("8. replace() 메소드를 이용하여 키에 대응하는 값 변환 (*기존 값을 올바르게 입력할 경우 가능)");
+		System.out.println("8. replace() 메서드를 이용하여 키에 대응하는 값 변환 (*기존 값을 올바르게 입력할 경우 가능)");
 		System.out.println();
 		System.out.println("* 불가능한 경우");
 		System.out.println("기존 'Blueberry'키의 값 : " + map.get("Blueberry"));
@@ -68,21 +68,21 @@ public class HashMap1_BasicTest {
 		System.out.println("변환 후 'Blueberry'키의 값 : " + map.get("Blueberry") + " - (변환O)");
 		System.out.println();
 		
-		System.out.println("9. containsKey() 메소드를 이용한 특정 키 포함 여부 확인");
+		System.out.println("9. containsKey() 메서드를 이용한 특정 키 포함 여부 확인");
 		System.out.println("Map이 'Banana'키를 포함하는가? " + map.containsKey("Banana"));
 		System.out.println("Map이 'Cherry'키를 포함하는가? " + map.containsKey("Cherry"));
 		System.out.println();
 		
-		System.out.println("10. containsValue() 메소드를 이용한 특정 값에 해당하는 키 포함 여부 확인");
+		System.out.println("10. containsValue() 메서드를 이용한 특정 값에 해당하는 키 포함 여부 확인");
 		System.out.println("Map에 값이 '1'인 키가 있는가? " + map.containsValue(1));
 		System.out.println("Map에 값이 '4'인 키가 있는가? " + map.containsValue(4));
 		System.out.println();
 		
-		System.out.println("11. isEmpty() & clear() 메소드를 이용한 전체 요소 제거");
-		//isEmpty() 메소드를 이용하여 맵이 비어있는지 확인
+		System.out.println("11. isEmpty() & clear() 메서드를 이용한 전체 요소 제거");
+		//isEmpty() 메서드를 이용하여 맵이 비어있는지 확인
 		System.out.println("Map이 비었는가? " + map.isEmpty());
-		System.out.println("clear() 메소드 실행");
-		map.clear(); //clear() 메소드를 이용한 전체 요소의 제거
+		System.out.println("clear() 메서드 실행");
+		map.clear(); //clear() 메서드를 이용한 전체 요소의 제거
 		System.out.println("Map이 비었는가? " + map.isEmpty());
 	}
 	
